70447498ccfc3ea4055bc603d46d0f9ee975f7d3
nicolledeavila <nicolledeavila3@gmail.com>
2024-10-09 02:30:46 -0500

    mañana se ve

--- NICOLLE_DE_AVILA/nicolle_de_avila_peluffo/POO/peticion.dart
+++ NICOLLE_DE_AVILA/nicolle_de_avila_peluffo/POO/peticion.dart
@@ -8,7 +8,6 @@ class Peticion {
   String? body;
 
   Peticion(Map map) {
-    //Map<dynamic, dynamic> map = jsonDecode();
     this.userId = map['userId'];
     this.id = map['id'];
     this.title = map['title'];
@@ -16,13 +15,13 @@ class Peticion {
   }
   
 }
-
-void main() async {
+   
+  void main() async {
   var url = Uri.https('jsonplaceholder.typicode.com', 'posts/1');
   var response = await http.get(url);
   
-  //String lili ='{"userId": 1,"id": 1,"title": "suntaut facere repellat provident occaecati excepturi optio reprehenderit","body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"}';
   Map<String, dynamic> map = jsonDecode(response.body);
+  
   Peticion peticion = new Peticion(map);
   
   print('Response status: ${response.statusCode}');
--- NICOLLE_DE_AVILA/nicolle_de_avila_peluffo/flutter/proyecto_flutter/lib/main.dart
+++ NICOLLE_DE_AVILA/nicolle_de_avila_peluffo/flutter/proyecto_flutter/lib/main.dart
@@ -9,29 +9,41 @@ class Myapp extends StatelessWidget {
     return MaterialApp(
         title: 'INFORMACION USUARIO',
         home: Scaffold(
-          appBar: AppBar(title: Text('USUARIO')),
+          backgroundColor:(Color.fromARGB(255, 3, 32, 56)),
+          appBar: AppBar(title: Text('USUARIO', textAlign: TextAlign.center,)),
           body: Column(children: [
-            Text('id: 1'),
-            Text('name: Leanne Graham'),
-            Text('username: Bret'),
-            Text('email: Sincere@april.biz'),
-            Text('DIRECCION:'),
-            Text('street: Kulas Light'),
-            Text('suite: Apt. 556'),
-            Text('city: Gwenborough'),
-            Text('zipcode: 92998-3874'),
-            Text('GEO:'),
-            Text('lat: -37.3159'),
-            Text('lng: 81.1496'),
-            Text('COMPANY:'),
-            Text('phone: 1-770-736-8031 x56442'),
-            Text('website: hildegard.org'),
-            Text('name: Romaguera-Crona'),
-            Text('catchPhrase: Multi-layered client-server neural-net'),
-            Text('bs: harness real-time e-markets'),
+            Container(
+              padding:EdgeInsets.all(18),
+              decoration:BoxDecoration(
+                border: Border.all(color: Color.fromARGB(255, 255, 255, 255)),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child:Text(
+                ('id: 1'),
+                Text('name: Leanne Graham'),
+                Text('username: Bret'),
+                Text('email: Sincere@april.biz'),
+                Text('DIRECCION:'),
+                Text('street: Kulas Light'),
+                Text('suite: Apt. 556'),
+                Text('city: Gwenborough'),
+                Text('zipcode: 92998-3874'),
+                Text('GEO:'),
+                Text('lat: -37.3159'),
+                Text('lng: 81.1496'),
+                Text('COMPANY:'),
+                Text('phone: 1-770-736-8031 x56442'),
+                Text('website: hildegard.org'),
+                Text('name: Romaguera-Crona'),
+                Text('catchPhrase: Multi-layered client-server neural-net'),
+                Text('bs: harness real-time e-markets'), 
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
             ElevatedButton(onPressed: () {}, child: Text('Buscar')),
             CircularProgressIndicator()
           ]),
-        ));
+        )
+      );
   }
 }
