e112311d709a6c7f32db15494c6720c288de5582
nicolledeavila <nicolledeavila3@gmail.com>
2024-12-03 14:03:41 -0500

    .

---
+++ NICOLLE_DE_AVILA/nicolle_de_avila_peluffo/trabajos_en_javascp/archivo1.js
@@ -0,0 +1,12 @@
+var a = document.querySelector("body > article > section > div > span:nth-child(1)")
+var b = document.querySelector("body > article > section > div > span:nth-child(4)")
+var c = document.querySelector("body > article > section > div > span:nth-child(7)")
+var d = document.querySelector("body > article > section > div > span:nth-child(10)")
+var e = document.querySelector("body > article > section > div > span:nth-child(13)")
+
+a.innerHTML = "  naty,"
+b.innerHTML = "apellido: perez,"
+c.innerHTML = "edad: 22,"
+d.innerHTML = " direccion: kr-14s#708,"
+e.innerHTML = " id: 123456"
+
