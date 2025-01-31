8a79231ec03adf79f337414ccfbca75057762b20
nicolledeavila <nicolledeavila3@gmail.com>
2025-01-31 17:42:27 -0500

    .

--- NICOLLE_DE_AVILA/nicolle_de_avila_peluffo/flutter_application_1/lib/screens/screen_home.dart
+++ NICOLLE_DE_AVILA/nicolle_de_avila_peluffo/flutter_application_1/lib/screens/screen_home.dart
@@ -13,26 +13,18 @@ class Homescreen extends StatelessWidget{
         backgroundColor: const Color.fromARGB(255, 0, 149, 255),
       ),
       body: Container(
-         margin: const EdgeInsets.only(top:10,left: 40),
-         width: 400,
-         height: 350,
-         decoration: const BoxDecoration(color: Color.fromARGB(255, 218, 176, 173)),
+         margin: const EdgeInsets.only(top: 20, bottom: 20, left: 20, right: 20),
+         width: 460,
+         height: 450,
+         decoration: const BoxDecoration(color: Color.fromARGB(255, 218, 176, 173),
+         ),
           child: Container(
-            padding: const EdgeInsets.all(10),
-            child: const Row(
-              children: [
-                CircleAvatar(
-                 backgroundColor:  Colors.blue,
-                  child: Text('j'),
-                ), SizedBox(
-                  width: 10,
-                ),
-                Text('ni',
-                style: TextStyle(color: Color.fromARGB(255, 205, 0, 0)),),
-                Text('co', style:TextStyle(fontSize: 20),),
-                Text('lle'),
-              ],
+            padding: const EdgeInsets.all(90),
+            child: DecoratedBox(decoration: BoxDecoration(border: Border.all(color: Colors.black,width: 2),
+            )
             ),
+            width: 10,
+            height: 10,
           ),
       ),
       backgroundColor: const Color.fromARGB(255, 233, 234, 233),
